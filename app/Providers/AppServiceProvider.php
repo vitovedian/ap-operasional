f38aa2ac06b02102917021c28e22b2c16574f05f
Vito Vedian <vitovedian@gmail.com>
2025-10-03 10:45:24 +0700

    fixed locale

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +25,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        $locale = config('app.locale', 'id');
+
+        App::setLocale($locale);
+        Date::setLocale($locale);
+        Carbon::setLocale($locale);
+        CarbonImmutable::setLocale($locale);
+
+        setlocale(LC_TIME, 'id_ID.UTF-8', 'id_ID', 'id', 'IND');
     }
 }
